4ef1a1f8e71d19dddef2f26ec1ab2fc23ae6d9c4
Tony Cleem <tonycleem@gmail.com>
2025-06-03 23:53:30 +0700

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.view_main_page)
+    path('', views.index, name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
