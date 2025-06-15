2b4f911008a6e3892355508278e1c733d6289051
Tony Cleem <tonycleem@gmail.com>
2025-06-15 13:59:39 +0700

    add README.md fix bags

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG_VALUE']
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
