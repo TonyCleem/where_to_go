5eeddc785bbb443424cb89451581d95c26f5412f
Tony Cleem <tonycleem@gmail.com>
2025-06-26 01:58:28 +0700

    add order in import

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,18 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
-from where_to_go import views
 from django.conf import settings
 from django.conf.urls.static import static
-from places.models import Location, Image
+from django.contrib import admin
+from django.urls import include, path
+
+from where_to_go import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('places/<int:place_id>/', views.location, name='location'),
-    path('places/<str:json_file>', views.get_json, name='get_json'),
+    path('places/<str:json_file>/', views.get_json, name='get_json'),
     path('tinymce/', include('tinymce.urls')),
-    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
