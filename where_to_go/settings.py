ad5214605e1c1c9c774ac8aa24651c0a4c6f5ff8
Tony Cleem <tonycleem@gmail.com>
2025-06-26 00:30:25 +0700

    delete HOST and PORT in db setting

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -60,8 +60,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'HOST': env.str('HOST'),
-        'PORT': env.int('PORT'),
     }
 }
 
